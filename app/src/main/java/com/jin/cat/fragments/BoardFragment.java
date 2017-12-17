e83b53db6c722adf0734fbc724d298894dbf2b6e
Jin104 <jd_104@naver.com>
2017-12-17 17:17:28 +0900

    검색

--- app/src/main/java/com/jin/cat/fragments/BoardFragment.java
+++ app/src/main/java/com/jin/cat/fragments/BoardFragment.java
@@ -2,6 +2,7 @@ package com.jin.cat.fragments;
 
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -21,10 +23,12 @@ import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.jin.cat.R;
 import com.jin.cat.activities.PostActivity;
 import com.jin.cat.activities.PostCreateActivity;
+import com.jin.cat.dialogs.PostSearchDialog;
 import com.jin.cat.models.Post;
 import com.jin.cat.utils.FirebaseUtils;
 import com.melnykov.fab.FloatingActionButton;
@@ -42,6 +46,9 @@ public class BoardFragment extends Fragment {
     private Button mButton1;
     private Button mButton2;
     private Button mButton3;
+    private ImageButton mSearchBtn;
+
+    private String postType;
 
     public BoardFragment() {
 
@@ -54,6 +61,7 @@ public class BoardFragment extends Fragment {
         mButton1 = (Button)mView.findViewById(R.id.board_all);
         mButton2 = (Button)mView.findViewById(R.id.board_qna);
         mButton3 = (Button)mView.findViewById(R.id.board_etc);
+        mSearchBtn = (ImageButton)mView.findViewById(R.id.board_search);
 
         mPostRecyclerView = (RecyclerView) mView.findViewById(R.id.board_recycler);
 
@@ -62,6 +70,7 @@ public class BoardFragment extends Fragment {
         layoutManager.setStackFromEnd(true);
         mPostRecyclerView.setLayoutManager(layoutManager);
 
+        postType="All";
         setupAdapter("All");
         mPostRecyclerView.setAdapter(mPostAdapter);
 
@@ -76,6 +85,7 @@ public class BoardFragment extends Fragment {
         mButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                postType="All";
                 setupAdapter("All");
                 mPostRecyclerView.setAdapter(mPostAdapter);
                 mButton1.setBackgroundColor(getContext().getResources().getColor(R.color.colorAccent));
@@ -87,6 +97,7 @@ public class BoardFragment extends Fragment {
         mButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                postType="질문";
                 setupAdapter("질문");
                 mPostRecyclerView.setAdapter(mPostAdapter);
                 mButton1.setBackgroundColor(getContext().getResources().getColor(R.color.colorGray4));
@@ -98,6 +109,7 @@ public class BoardFragment extends Fragment {
         mButton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                postType="기타";
                 setupAdapter("기타");
                 mPostRecyclerView.setAdapter(mPostAdapter);
                 mButton1.setBackgroundColor(getContext().getResources().getColor(R.color.colorGray4));
@@ -105,9 +117,93 @@ public class BoardFragment extends Fragment {
                 mButton3.setBackgroundColor(getContext().getResources().getColor(R.color.colorAccent));
             }
         });
+
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("postType", postType);
+                PostSearchDialog dialog = new PostSearchDialog();
+                dialog.setArguments(bundle);
+                dialog.setTargetFragment(BoardFragment.this, 0);
+                dialog.show(getFragmentManager(), null);
+
+                //dialog.show(getFragmentManager(), null);
+
+            }
+        });
         return mView;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String search = data.getExtras().getString("search");
+        String type = data.getExtras().getString("searchType");
+
+
+        //Toast.makeText(getActivity(), str, Toast.LENGTH_SHORT).show();
+        setupAdapter1(type, search);
+        mPostRecyclerView.setAdapter(mPostAdapter);
+    }
+
+    public void setupAdapter1(String type, String search) {
+
+//        mAuth.addAuthStateListener(mAuthListener);
+        Query query = null;
+        if(type.equals("postDesc")){
+            //query = FirebaseUtils.getPostRef().child(postType).orderByChild(type+"/"+type).startAt(search);
+            query = FirebaseUtils.getPostRef().child(postType).orderByChild(type).startAt(search).endAt(search+"\uf8ff");
+        }else if(type.equals("postTitle")){
+            query = FirebaseUtils.getPostRef().child(postType).orderByChild(type).startAt(search).endAt(search+"\uf8ff");
+        }
+        mPostAdapter = new FirebaseRecyclerAdapter<Post, PostViewHolder>(
+                Post.class,
+                R.layout.row_board,
+                PostViewHolder.class,
+                query
+        ) {
+            @Override
+            protected void populateViewHolder(PostViewHolder viewHolder, final Post model, final int position) {
+
+                viewHolder.setNumComments(String.valueOf(model.getNumComments()));
+                viewHolder.setTime(DateUtils.getRelativeTimeSpanString(model.getTimeCreated()));
+                viewHolder.setUsername(model.getUser().getUser());
+                viewHolder.setPostTitle(model.getPostTitle());
+
+                Glide.with(getActivity())
+                        .load(model.getUser().getImageUrl())
+                        .into(viewHolder.postImage);
+
+                if (model.getPostImageUrl() != null) {
+                    viewHolder.postImage.setVisibility(View.VISIBLE);
+                    viewHolder.setImage(getActivity(),model.getPostImageUrl());
+                } else {
+                    viewHolder.postImage.setImageBitmap(null);
+                    viewHolder.postImage.setVisibility(View.GONE);
+                }
+
+
+                viewHolder.linearLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getContext(), PostActivity.class);
+                        intent.putExtra("postID", model.getPostId());
+                        intent.putExtra("postType", model.getPostType());
+                        startActivity(intent);
+                    }
+                });
+
+            }
+        };
+
+        if(query==null){
+            Toast.makeText(getActivity(), "결과 없음", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
     public void setupAdapter(String str) {
 
@@ -150,78 +246,6 @@ public class BoardFragment extends Fragment {
                         startActivity(intent);
                     }
                 });
-//                viewHolder.postCommentLayout.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Intent intent = new Intent(getContext(), PostCreateActivity.class);
-//                        intent.putExtra(FirebaseUtils.Constants.EXTRA_POST, model);
-//                        startActivity(intent);
-//                    }
-//                });
-
-//                final String post_key = getRef(position).getKey().toString();
-//
-//                viewHolder.setTitle(model.getPostText());
-//                viewHolder.setDesc(model.getPostText());
-//                viewHolder.setImage(getActivity(), model.getPostImageUrl());
-//                //viewHolder.setUserName(model.getUsername());
-//
-//                viewHolder.setLikeBtn(post_key);
-//
-//                viewHolder.mView.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Intent singlePostActivity = new Intent(getActivity(), SinglePostActivity.class);
-//                        singlePostActivity.putExtra("PostId", post_key);
-//                        startActivity(singlePostActivity);
-//                    }
-//                });
-//
-//                viewHolder.mLikeBtn.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//
-//                        mProcessLike = true;
-//
-//                        if (mProcessLike) {
-//                            mDatabaseLike.addValueEventListener(new ValueEventListener() {
-//                                @Override
-//                                public void onDataChange(DataSnapshot dataSnapshot) {
-//
-//                                    if (mProcessLike) {
-//
-//                                        if (dataSnapshot.child(post_key).hasChild(mAuth.getCurrentUser().getUid())) {
-//
-//                                            mDatabaseLike.child(post_key).child(mAuth.getCurrentUser().getUid()).removeValue();
-//                                            mProcessLike = false;
-//
-//                                        } else {
-//
-//                                            mDatabaseLike.child(post_key).child(mAuth.getCurrentUser().getUid()).setValue("RandomValue");
-//                                            mProcessLike = false;
-//
-//                                        }
-//                                    }
-//                                }
-//
-//                                @Override
-//                                public void onCancelled(DatabaseError databaseError) {
-//
-//                                }
-//                            });
-//                        }
-//                    }
-//                });
-//
-//                viewHolder.mCommentBtn.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Intent intent = new Intent(getActivity(), CommentActivity.class);
-//                        intent.putExtra("PostId", post_key);
-//                        intent.putExtra("UserId", mAuth.getCurrentUser().getUid());
-//                        startActivity(intent);
-//                    }
-//                });
 
             }
         };
